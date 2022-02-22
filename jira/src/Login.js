25b2054f1d1bc46715fb9ead81eacd4560081625
Achyutha Harish <aharish@pdx.edu>
2022-02-22 13:26:18 -0800

    first commit

--- jira/src/Login.js
+++ jira/src/Login.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Container } from "react-bootstrap"
 
 const AUTH_URL =
-"https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=TmLEwt038NnvwQNnQV0CA88DYbTu7VUm&scope=read%3Ajira-user%20write%3Ajira-work%20read%3Ajira-work%20manage%3Ajira-webhook%20manage%3Ajira-project&redirect_uri=http%3A%2F%2Flocalhost%3A3000&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent"
+"https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=QEPbDfbzRgDNS2P5nROTNKBenrdNWLRA&scope=read%3Ajira-user%20read%3Ajira-work%20write%3Ajira-work%20manage%3Ajira-project&redirect_uri=http%3A%2F%2Flocalhost%3A3000&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent"
 
 export default function Login() {
   return (
