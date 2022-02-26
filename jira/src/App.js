d02d95efb33417287550cc0bd4df17915458cb60
ArpithaSrinivas <89316938+ArpithaSrinivas5@users.noreply.github.com>
2022-02-25 23:38:29 -0800

    Update App.js

--- jira/src/App.js
+++ jira/src/App.js
@@ -2,24 +2,22 @@ import Login from "./Login"
 import Dashboard from "./Dashboard"
 import { render } from "react-dom";
 import {BrowserRouter,Routes,Route,} from "react-router-dom";
-import Home from  "./home"
+import Home from  "./components/home"
 
 
-/*
 const code = new URLSearchParams(window.location.search).get("code")
 function App() {
   
-    return code ? <Dashboard code={code} /> : <Login />
-}
-export default App; */
+    // return code ? <Dashboard code={code} /> : <Login />
 
-render(
+
+return(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home  />} />
-      <Route path="home" element={<Home />} />
+     
+      <Route path="/home" element={< Home />} />
     </Routes>
   </BrowserRouter>
 );
-
+}
 export default App;
